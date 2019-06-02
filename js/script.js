01245872b239e5ae5d6fe5656e79c3201c31a7c9
AlexeyBalabaev <jhon.snow.verysnow@yandex.ru>
2019-06-02 21:49:14 +0300

    Edits

--- js/script.js
+++ js/script.js
@@ -17,6 +17,8 @@
 			limit: 30
 		};
 
+	};
+
 		this.id = id;
 		this.setup = setup;
 
@@ -38,5 +40,50 @@
 	window.Gallery = Gallery;
 
 	var fn = Gallery.prototype;
+
+	fn.init = function() {
+		this.options = extend({}, this.defaults, this.setup);
+
+		this.setSizeCarousel();
+		this.setCoordinates();
+		this.initControl();
+		if(!this.events) {
+			this.registerEvents();
+		}
+	};
+
+	fn.setSizeCarousel = function() {
+		this.widthSlider = this.slider.offsetWidth;
+		this.max = this.count - this.options.visibleItems;
+
+		var width = (this.widthSlider - this.options.margin * (this.options.visibleItems - 1)) / this.options.visibleItems;
+
+		this.width = width + this.options.margin;
+		this.widths = this.width * this.count;
+		this.stage.style.width = this.widths + 'px';
+
+		[].forEach.call(this.items, function(el) {
+			el.style.cssText = 'width' + width + 'px; margin-right:' + this.options.margin + 'px;';
+		}.bind(this));
+
+		this.gallery.style.visibility = 'visible';
+	};
+
+	fn.setCoordinates = function() {
+		var point = 0;
+	};
+
+	function extend(out) {
+		out = out || {};
+		for (var i = 1; i < arguments.length; i++) {
+			if (!arguments[i]) continue;
+
+			for (var key in arguments[i]) {
+				if (arguments[i].hasOwnProperty(key)) 
+					out[key] = arguments[i][key];
+			}
+		}
+		return out;
+	};
 
 }) ();
